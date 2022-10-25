54e27314e9bbb76b2b883db13adbb7fa1139fff8
Sergey Borovkov <serge.borovkov@gmail.com>
2022-10-25 16:41:18 +0400

    Initial commit.
    Login is implemented. Token is verified as working when making a request to get a non existing group.
    There are tests that check that response for correct and incorrect token is handled.
    In case of correct token path $HOME/.screenly is checked to have the token stored upon successful login.

---
+++ src/commands.rs
@@ -0,0 +1,21 @@
+use crate::Authentication;
+
+
+
+
+
+
+
+pub struct ScreenCommand {
+    authentication: Authentication,
+}
+
+impl ScreenCommand {
+    fn new(authentication: Authentication) -> Self {
+        Self { authentication }
+    }
+
+    fn list() {}
+
+    fn get(_id: &str) {}
+}
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+mod authentication;
+mod commands;
+
+use crate::authentication::{Authentication, AuthenticationError};
+use clap::{command, Parser, Subcommand};
+use log::{warn};
+use simple_logger::SimpleLogger;
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Logins with the token and stores it for further use if it's valid
+    Login { token: String },
+    /// Screen related commands
+    #[command(subcommand)]
+    Screen(ScreenCommands),
+}
+
+#[derive(Subcommand, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum ScreenCommands {
+    /// Lists your screens
+    List,
+    /// Gets a single screen by id
+    Get { id: String },
+}
+
+fn main() {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .unwrap();
+    let cli = Cli::parse();
+    let authentication = Authentication::new();
+    match &cli.command {
+        Commands::Login { token } => match authentication.verify_and_store_token(token) {
+            Ok(()) => {
+                println!("Login credentials have been saved.");
+                std::process::exit(0);
+            }
+
+            Err(e) => match e {
+                AuthenticationError::ConnectionError => {
+                    println!("Connection error. Try again later.");
+                    std::process::exit(1);
+                }
+                AuthenticationError::WrongCredentialsError => {
+                    println!("Token verification failed.");
+                    std::process::exit(2);
+                }
+                _ => {
+                    println!("Unknown error");
+                    std::process::exit(2);
+                }
+            },
+        },
+        Commands::Screen(command) => match command {
+            ScreenCommands::List => {
+                warn!("List");
+            }
+            ScreenCommands::Get { id: _ } => {
+                warn!("GET");
+            }
+        },
+    }
+    warn!("Hello, world!");
+}
