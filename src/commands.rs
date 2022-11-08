00d870cbc4f1454a9e86647e1c5344d290acd3b4
Sergey Borovkov <serge.borovkov@gmail.com>
2022-11-08 13:20:31 +0400

    Convert uptime to human readable duration

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,7 @@
 use crate::{Authentication, AuthenticationError};
 
-
+use humantime::format_duration;
+use std::time::Duration;
 use thiserror::Error;
 
 use prettytable::{row, Table};
@@ -43,12 +44,13 @@ impl Formatter for Screens {
             OutputType::HumanReadable => {
                 let mut table = Table::new();
                 table.add_row(row!(
+                    b =>
                     "Id",
                     "Name",
-                    "Hardware version",
-                    "In sync",
-                    "Last ping",
-                    "Uptime (seconds)"
+                    "Hardware Version",
+                    "In Sync",
+                    "Last Ping",
+                    "Uptime"
                 ));
 
                 if !self.value.is_array() {
@@ -57,13 +59,32 @@ impl Formatter for Screens {
 
                 if let Some(screens) = self.value.as_array() {
                     for screen in screens {
+                        let formatted_uptime = if let Some(uptime) = screen["uptime"].as_str() {
+                            if uptime.eq("N/A") {
+                                uptime.to_owned()
+                            } else {
+                                format_duration(Duration::new(
+                                    uptime.parse::<f64>().unwrap_or(0.0) as u64,
+                                    0,
+                                ))
+                                .to_string()
+                            }
+                        } else {
+                            "N/A".to_owned()
+                        };
+
                         table.add_row(row!(
-                            screen["id"].as_str().unwrap_or("n/a"),
-                            screen["name"].as_str().unwrap_or("n/a"),
-                            screen["hardware_version"].as_str().unwrap_or("n/a"),
-                            screen["in_sync"].as_bool().unwrap_or(false),
-                            screen["last_ping"].as_str().unwrap_or("n/a"),
-                            screen["uptime"].as_str().unwrap_or("n/a"),
+                            screen["id"].as_str().unwrap_or("N/A"),
+                            screen["name"].as_str().unwrap_or("N/A"),
+                            screen["hardware_version"].as_str().unwrap_or("N/A"),
+                            // converting to &str just to make sure they are capitalized
+                            if screen["in_sync"].as_bool().unwrap_or(false) {
+                                "True"
+                            } else {
+                                "False"
+                            },
+                            screen["last_ping"].as_str().unwrap_or("N/A"),
+                            formatted_uptime,
                         ));
                     }
                 }
@@ -202,16 +223,16 @@ mod tests {
     #[test]
     fn test_format_when_human_readable_output_is_set_should_return_correct_formatted_string() {
         let screen = Screens::new(serde_json::from_str("[{\"id\":\"017a5104-524b-33d8-8026-9087b59e7eb5\",\"team_id\":\"016343c2-82b8-0000-a121-e30f1035875e\",\"created_at\":\"2021-06-28T05:07:55+00:00\",\"name\":\"Renat's integrated wired NM\",\"is_enabled\":true,\"coords\":[55.22931, 48.90429],\"last_ping\":\"2021-08-25T06:17:20.728+00:00\",\"last_ip\":null,\"local_ip\":\"192.168.1.146\",\"mac\":\"b8:27:eb:d6:83:6f\",\"last_screenshot_time\":\"2021-08-25T06:09:04.399+00:00\",\"uptime\":\"230728.38\",\"load_avg\":\"0.14\",\"signal_strength\":null,\"interface\":\"eth0\",\"debug\":false,\"location\":\"Kamsko-Ust'inskiy rayon, Russia\",\"team\":\"016343c2-82b8-0000-a121-e30f1035875e\",\"timezone\":\"Europe/Moscow\",\"type\":\"hardware\",\"hostname\":\"srly-4shnfrdc5cd2p0p\",\"ws_open\":false,\"status\":\"Offline\",\"last_screenshot\":\"https://us-assets.screenlyapp.com/01CD1W50NR000A28F31W83B1TY/screenshots/01F98G8MJB6FC809MGGYTSWZNN/5267668e6db35498e61b83d4c702dbe8\",\"in_sync\":false,\"software_version\":\"Screenly 2 Player\",\"hardware_version\":\"Raspberry Pi 3B\",\"config\":{\"hdmi_mode\": 34, \"hdmi_boost\": 2, \"hdmi_drive\": 0, \"hdmi_group\": 0, \"verify_ssl\": true, \"audio_output\": \"hdmi\", \"hdmi_timings\": \"\", \"overscan_top\": 0, \"overscan_left\": 0, \"use_composite\": false, \"display_rotate\": 0, \"overscan_right\": 0, \"overscan_scale\": 0, \"overscan_bottom\": 0, \"disable_overscan\": 0, \"shuffle_playlist\": false, \"framebuffer_width\": 0, \"use_composite_pal\": false, \"framebuffer_height\": 0, \"hdmi_force_hotplug\": true, \"use_composite_ntsc\": false, \"hdmi_pixel_encoding\": 0, \"play_history_enabled\": false}}, {\"id\":\"017a5104-524b-33d8-8026-9087b59e7eb6\",\"team_id\":\"016343c2-82b8-0000-a121-e30f1035875d\",\"created_at\":\"2020-06-28T05:07:55+00:00\",\"name\":\"Not Renat's integrated wired NM\",\"is_enabled\":true,\"coords\":[55.22931, 48.90429],\"last_ping\":\"2020-08-25T06:17:20.728+00:00\",\"last_ip\":null,\"local_ip\":\"192.168.1.146\",\"mac\":\"b8:27:eb:d6:83:6f\",\"last_screenshot_time\":\"2021-08-25T06:09:04.399+00:00\",\"uptime\":\"230728.38\",\"load_avg\":\"0.14\",\"signal_strength\":null,\"interface\":\"eth0\",\"debug\":false,\"location\":\"Kamsko-Ust'inskiy rayon, Russia\",\"team\":\"016343c2-82b8-0000-a121-e30f1035875e\",\"timezone\":\"Europe/Moscow\",\"type\":\"hardware\",\"hostname\":\"srly-4shnfrdc5cd2p0p\",\"ws_open\":false,\"status\":\"Offline\",\"last_screenshot\":\"https://us-assets.screenlyapp.com/01CD1W50NR000A28F31W83B1TY/screenshots/01F98G8MJB6FC809MGGYTSWZNN/5267668e6db35498e61b83d4c702dbe8\",\"in_sync\":false,\"software_version\":\"Screenly 2 Player\",\"hardware_version\":\"Raspberry Pi 3B\",\"config\":{\"hdmi_mode\": 34, \"hdmi_boost\": 2, \"hdmi_drive\": 0, \"hdmi_group\": 0, \"verify_ssl\": true, \"audio_output\": \"hdmi\", \"hdmi_timings\": \"\", \"overscan_top\": 0, \"overscan_left\": 0, \"use_composite\": false, \"display_rotate\": 0, \"overscan_right\": 0, \"overscan_scale\": 0, \"overscan_bottom\": 0, \"disable_overscan\": 0, \"shuffle_playlist\": false, \"framebuffer_width\": 0, \"use_composite_pal\": false, \"framebuffer_height\": 0, \"hdmi_force_hotplug\": true, \"use_composite_ntsc\": false, \"hdmi_pixel_encoding\": 0, \"play_history_enabled\": false}}]").unwrap());
-
+        println!("{}", screen.format(OutputType::HumanReadable));
         let expected_output = concat!(
 "+--------------------------------------+---------------------------------+------------------+---------+-------------------------------+------------------+\n",
-"| Id                                   | Name                            | Hardware version | In sync | Last ping                     | Uptime (seconds) |\n",
+"| Id                                   | Name                            | Hardware Version | In Sync | Last Ping                     | Uptime           |\n",
 "+--------------------------------------+---------------------------------+------------------+---------+-------------------------------+------------------+\n",
-"| 017a5104-524b-33d8-8026-9087b59e7eb5 | Renat's integrated wired NM     | Raspberry Pi 3B  | false   | 2021-08-25T06:17:20.728+00:00 | 230728.38        |\n",
+"| 017a5104-524b-33d8-8026-9087b59e7eb5 | Renat's integrated wired NM     | Raspberry Pi 3B  | False   | 2021-08-25T06:17:20.728+00:00 | 2days 16h 5m 28s |\n",
 "+--------------------------------------+---------------------------------+------------------+---------+-------------------------------+------------------+\n",
-"| 017a5104-524b-33d8-8026-9087b59e7eb6 | Not Renat's integrated wired NM | Raspberry Pi 3B  | false   | 2020-08-25T06:17:20.728+00:00 | 230728.38        |\n",
-"+--------------------------------------+---------------------------------+------------------+---------+-------------------------------+------------------+\n");
-
+"| 017a5104-524b-33d8-8026-9087b59e7eb6 | Not Renat's integrated wired NM | Raspberry Pi 3B  | False   | 2020-08-25T06:17:20.728+00:00 | 2days 16h 5m 28s |\n",
+"+--------------------------------------+---------------------------------+------------------+---------+-------------------------------+------------------+\n"
+);
         assert_eq!(screen.format(OutputType::HumanReadable), expected_output);
     }
 }
