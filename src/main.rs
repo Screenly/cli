4d870a9773e1568d6080a1a1adf708331d033d48
Stanislaw Iwan <siwan@screenly.io>
2023-07-05 22:31:16 +0200

    Enable Sentry for CLI

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,17 @@ extern crate prettytable;
 use crate::authentication::{Authentication, AuthenticationError};
 use clap::Parser;
 use simple_logger::SimpleLogger;
+use sentry_anyhow::capture_anyhow;
 
 fn main() {
     SimpleLogger::new().init().unwrap();
+
+    let _sentry_dns = "https://cf4fbc3a05024138ad8ef56f218fbb0e@o1402263.ingest.sentry.io/4505391836233728";
+
+    let _guard = sentry::init((_sentry_dns, sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+    }));
 
     let cli = cli::Cli::parse();
     cli::handle_cli(&cli);
