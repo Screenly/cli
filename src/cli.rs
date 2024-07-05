774dd9890329f88ff6af04276376499c51f41066
rusko124 <psafronov@screenly.io>
2024-07-05 13:39:40 +0400

    New: generated help markdown documentation (#176)
    * New: generated help markdown documentation
    
    * Deleted: extra file
    
    * Corrupt docs/CommandLineHelp.md for testing
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Edits: .github/workflows/docs.yml
    
    * Revert back docs/CommandLineHelp.md
    
    * Edits: package name
    
    * Fix: linter warning
    
    * Remove: extra code
    
    * Update src/cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -63,6 +63,9 @@ pub enum Commands {
     /// Edge App related commands.
     #[command(subcommand)]
     EdgeApp(EdgeAppCommands),
+    /// For generating `docs/CommandLineHelp.md`.
+    #[clap(hide = true)]
+    PrintHelpMarkdown {},
 }
 
 #[derive(Subcommand, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -576,6 +579,9 @@ pub fn handle_cli(cli: &Cli) {
             Authentication::remove_token().expect("Failed to remove token.");
             info!("Logout successful.");
             std::process::exit(0);
+        }
+        Commands::PrintHelpMarkdown {} => {
+            clap_markdown::print_help_markdown::<Cli>();
         }
     }
 }
