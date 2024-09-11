46ae998d40a95b901fa3ebf5a308522d5790de09
Sergey Borovkov <serge.borovkov@gmail.com>
2024-09-11 17:30:46 +0400

    Adds ready for rendering function in the emulator.
    It does nothing in the emulator.

--- src/commands/edge_app/server.rs
+++ src/commands/edge_app/server.rs
@@ -155,7 +155,7 @@ fn format_js(data: MockData, secrets: &[(String, Value)]) -> String {
     settings.sort_by_key(|a| a.0.clone());
 
     format!(
-        "var screenly = {{\n{metadata},\n{settings},\n{cors_proxy}\n}};",
+        "var screenly = {{\n{metadata},\n{settings},\n{cors_proxy},\n    signalReadyForRendering: function() {{}}\n}};",
         metadata = format_section("metadata", &hashmap_from_metadata(&data.metadata)),
         settings = format_section("settings", &settings),
         cors_proxy = "    cors_proxy_url: \"http://127.0.0.1:8080\""
@@ -395,7 +395,8 @@ settings:
         "override_timezone": "",
         "tag_manager_id": ""
     },
-    cors_proxy_url: "http://127.0.0.1:8080"
+    cors_proxy_url: "http://127.0.0.1:8080",
+    signalReadyForRendering: function() {}
 };"#;
         assert_eq!(content, expected_content);
     }
