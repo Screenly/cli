f98a7d79644be567a106a8959bbaacfec777b9aa
Sergey Borovkov <serge.borovkov@gmail.com>
2025-08-05 18:48:03 +0400

    Try to update nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Screenly CLI flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs }:
